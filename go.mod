b047966b483a9340213c956ed34c7b3fd0e6bbe6
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2023-06-25 01:35:24 +0600

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module bookstore
+
+go 1.16
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jinzhu/gorm v1.9.16
+	gorm.io/gorm v1.25.1
+)
---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"bookstore/pkg/controllers"
+)
+
+var RegisterBookstoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/",controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/",controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}",controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}",controllers.UpdateBook).Methods("PUT")
+}
